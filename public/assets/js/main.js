381e0cbfd68636c3cbddb5707a4b26b6a03135fc
Adi Kancherla <findadit@gmail.com>
2018-11-15 05:35:14 -0800

    removed full page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -140,15 +140,6 @@
           $('.job-desc').toggle();
       });
 
-      $('#fullpage').fullpage({
-        //options here
-        autoScrolling:true,
-        scrollHorizontally: false
-      });
-
-      //methods
-      $.fn.fullpage.setAllowScrolling(true);
-
   });      
 
 }(jQuery));
